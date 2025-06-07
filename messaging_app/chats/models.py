b9707f1e597a5ed464f5c9ce592321f36b4abf9f
jbmensah <jaybmensah@gmail.com>
2025-06-07 19:41:04 +0000

    Task 1: Add phone_number field to CustomUser mode

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -9,6 +9,7 @@ class CustomUser(AbstractUser):
     password = models.CharField(max_length=128)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.username
